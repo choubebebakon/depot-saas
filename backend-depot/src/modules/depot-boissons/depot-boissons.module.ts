29ac86438dc23910976a79ff8d0f7e8b25382ff2
choubebebakon <choubebebakon@gmail.com>
2026-08-31 13:53:24 +0100

    feat(promotions): enregistrer module promotions dépôt boissons

--- backend-depot/src/modules/depot-boissons/depot-boissons.module.ts
+++ backend-depot/src/modules/depot-boissons/depot-boissons.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma.module';
 import { DepotBoissonsController } from './depot-boissons.controller';
 import { DepotBoissonsService } from './depot-boissons.service';
+import { DepotBoissonsPromotionsController } from './promotions.controller';
+import { DepotBoissonsPromotionsService } from './promotions.service';
 import { CaisseModule } from '../../caisse/caisse.module';
 import { AuditModule } from '../../audit/audit.module';
 
 @Module({
   imports: [PrismaModule, CaisseModule, AuditModule],
-  controllers: [DepotBoissonsController],
-  providers: [DepotBoissonsService],
-  exports: [DepotBoissonsService],
+  controllers: [DepotBoissonsController, DepotBoissonsPromotionsController],
+  providers: [DepotBoissonsService, DepotBoissonsPromotionsService],
+  exports: [DepotBoissonsService, DepotBoissonsPromotionsService],
 })
 export class DepotBoissonsModule {}
