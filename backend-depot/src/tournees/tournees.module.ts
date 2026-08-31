671214e0e950f82f5dd3f81964bd415a04dccdb1
choubebebakon <choubebebakon@gmail.com>
2026-08-31 17:23:46 +0100

    feat(tournees): register scoped tricycle edition

--- backend-depot/src/tournees/tournees.module.ts
+++ backend-depot/src/tournees/tournees.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TourneesService } from './tournees.service';
 import { TourneesController } from './tournees.controller';
+import { TricycleEditController } from './tricycle-edit.controller';
+import { TricycleEditService } from './tricycle-edit.service';
 import { PrismaService } from '../prisma.service';
 
 @Module({
-  controllers: [TourneesController],
-  providers: [TourneesService, PrismaService],
-  exports: [TourneesService],
+  controllers: [TourneesController, TricycleEditController],
+  providers: [TourneesService, TricycleEditService, PrismaService],
+  exports: [TourneesService, TricycleEditService],
 })
 export class TourneesModule {}
