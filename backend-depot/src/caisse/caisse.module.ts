c3b36839e4a5ff09155f445041d76428a6b20136
choubebebakon <choubebebakon@gmail.com>
2026-08-31 03:39:51 +0100

    fix(phase2): remove duplicate depot scope provider from caisse module

--- backend-depot/src/caisse/caisse.module.ts
+++ backend-depot/src/caisse/caisse.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { CaisseService } from './caisse.service';
 import { CaisseController } from './caisse.controller';
 import { PrismaService } from '../prisma.service';
-import { DepotScopeService } from '../common/depot-scope.service';
 
 @Module({
   controllers: [CaisseController],
-  providers: [CaisseService, PrismaService, DepotScopeService],
+  providers: [CaisseService, PrismaService],
   exports: [CaisseService],
 })
 export class CaisseModule {}
