9d996fe0c156fe368294f3a021174804574a4db6
choubebebakon <choubebebakon@gmail.com>
2026-08-31 03:39:26 +0100

    fix(phase2): expose validated depot scope to caisse module

--- backend-depot/src/caisse/caisse.module.ts
+++ backend-depot/src/caisse/caisse.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CaisseService } from './caisse.service';
 import { CaisseController } from './caisse.controller';
 import { PrismaService } from '../prisma.service';
+import { DepotScopeService } from '../common/depot-scope.service';
 
 @Module({
   controllers: [CaisseController],
-  providers: [CaisseService, PrismaService],
+  providers: [CaisseService, PrismaService, DepotScopeService],
   exports: [CaisseService],
 })
 export class CaisseModule {}
