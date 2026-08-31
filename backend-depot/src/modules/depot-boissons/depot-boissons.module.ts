6f51007148056488503529a05862a515918fe07a
choubebebakon <choubebebakon@gmail.com>
2026-08-31 17:21:32 +0100

    feat(tournees): register scoped edition service and controller

--- backend-depot/src/modules/depot-boissons/depot-boissons.module.ts
+++ backend-depot/src/modules/depot-boissons/depot-boissons.module.ts
@@ -4,13 +4,27 @@ import { DepotBoissonsController } from './depot-boissons.controller';
 import { DepotBoissonsService } from './depot-boissons.service';
 import { DepotBoissonsPromotionsController } from './promotions.controller';
 import { DepotBoissonsPromotionsService } from './promotions.service';
+import { DepotBoissonsTourneesEditController } from './tournees-edit.controller';
+import { DepotBoissonsTourneesEditService } from './tournees-edit.service';
 import { CaisseModule } from '../../caisse/caisse.module';
 import { AuditModule } from '../../audit/audit.module';
 
 @Module({
   imports: [PrismaModule, CaisseModule, AuditModule],
-  controllers: [DepotBoissonsController, DepotBoissonsPromotionsController],
-  providers: [DepotBoissonsService, DepotBoissonsPromotionsService],
-  exports: [DepotBoissonsService, DepotBoissonsPromotionsService],
+  controllers: [
+    DepotBoissonsController,
+    DepotBoissonsPromotionsController,
+    DepotBoissonsTourneesEditController,
+  ],
+  providers: [
+    DepotBoissonsService,
+    DepotBoissonsPromotionsService,
+    DepotBoissonsTourneesEditService,
+  ],
+  exports: [
+    DepotBoissonsService,
+    DepotBoissonsPromotionsService,
+    DepotBoissonsTourneesEditService,
+  ],
 })
 export class DepotBoissonsModule {}
