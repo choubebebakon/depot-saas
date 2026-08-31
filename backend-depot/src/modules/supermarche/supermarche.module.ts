0d3d287426eb61fa1ff2eecf0075da50ec17f2ee
choubebebakon <choubebebakon@gmail.com>
2026-08-31 03:39:17 +0100

    fix(phase2): wire authoritative supermarket POS service

--- backend-depot/src/modules/supermarche/supermarche.module.ts
+++ backend-depot/src/modules/supermarche/supermarche.module.ts
@@ -3,11 +3,18 @@ import { PrismaModule } from '../../prisma.module';
 import { AuditModule } from '../../audit/audit.module';
 import { SupermarcheController } from './supermarche.controller';
 import { SupermarcheService } from './supermarche.service';
+import { SupermarchePosService } from './supermarche-pos.service';
 
 @Module({
   imports: [PrismaModule, AuditModule],
   controllers: [SupermarcheController],
-  providers: [SupermarcheService],
+  providers: [
+    SupermarchePosService,
+    {
+      provide: SupermarcheService,
+      useExisting: SupermarchePosService,
+    },
+  ],
   exports: [SupermarcheService],
 })
 export class SupermarcheModule {}
