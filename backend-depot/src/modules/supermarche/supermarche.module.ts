b75832f42e40fad6050070dc9b0ff78c2818a5c8
choubebebakon <choubebebakon@gmail.com>
2026-09-02 13:42:07 +0100

    fix(stock): bind production supermarket stock service

--- backend-depot/src/modules/supermarche/supermarche.module.ts
+++ backend-depot/src/modules/supermarche/supermarche.module.ts
@@ -4,15 +4,17 @@ import { AuditModule } from '../../audit/audit.module';
 import { SupermarcheController } from './supermarche.controller';
 import { SupermarcheService } from './supermarche.service';
 import { SupermarchePosService } from './supermarche-pos.service';
+import { ProductionSupermarcheStockService } from './production-stock.service';
 
 @Module({
   imports: [PrismaModule, AuditModule],
   controllers: [SupermarcheController],
   providers: [
+    ProductionSupermarcheStockService,
     SupermarchePosService,
     {
       provide: SupermarcheService,
-      useExisting: SupermarchePosService,
+      useExisting: ProductionSupermarcheStockService,
     },
   ],
   exports: [SupermarcheService],
