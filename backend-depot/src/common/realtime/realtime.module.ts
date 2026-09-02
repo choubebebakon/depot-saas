9f05434274985bee525ef560e795615a85eaf7bd
choubebebakon <choubebebakon@gmail.com>
2026-09-03 00:17:50 +0100

    fix(realtime): authorize active depot rooms for multi-depot users

--- backend-depot/src/common/realtime/realtime.module.ts
+++ backend-depot/src/common/realtime/realtime.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../../auth/auth.module';
+import { DepotsModule } from '../../depots/depots.module';
 import { DepotScopeService } from '../depot-scope.service';
 import { RealtimeGateway } from './realtime.gateway';
 import { RealtimeMutationInterceptor } from './realtime-mutation.interceptor';
 import { RealtimeService } from './realtime.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, DepotsModule],
   providers: [
     RealtimeGateway,
     RealtimeService,
