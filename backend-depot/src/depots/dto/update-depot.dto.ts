d50d2ea6b9a841e2b8d0ec0656bd903635a3d29b
choubebebakon <choubebebakon@gmail.com>
2026-09-03 13:15:15 +0100

    fix(depôts): limiter les champs modifiables d'une configuration

--- backend-depot/src/depots/dto/update-depot.dto.ts
+++ backend-depot/src/depots/dto/update-depot.dto.ts
@@ -1,4 +1,31 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDepotDto } from './create-depot.dto';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class UpdateDepotDto extends PartialType(CreateDepotDto) {}
+export class UpdateDepotDto {
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(120)
+  nom?: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(255)
+  adresse?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(120)
+  emplacement?: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  @MaxLength(20)
+  codePrefix?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isArchived?: boolean;
+}
