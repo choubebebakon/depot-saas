f96447ef18ffcdb06241926ffd39ee5d4f0ee4d8
choubebebakon <choubebebakon@gmail.com>
2026-09-03 03:33:14 +0100

    fix(realtime): provide database authority for websocket sessions

--- backend-depot/src/common/realtime/realtime.module.ts
+++ backend-depot/src/common/realtime/realtime.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../../auth/auth.module';
 import { DepotsModule } from '../../depots/depots.module';
 import { DepotScopeService } from '../depot-scope.service';
+import { PrismaService } from '../../prisma.service';
 import { RealtimeGateway } from './realtime.gateway';
 import { RealtimeMutationInterceptor } from './realtime-mutation.interceptor';
 import { RealtimeService } from './realtime.service';
@@ -9,6 +10,7 @@ import { RealtimeService } from './realtime.service';
 @Module({
   imports: [AuthModule, DepotsModule],
   providers: [
+    PrismaService,
     RealtimeGateway,
     RealtimeService,
     RealtimeMutationInterceptor,
