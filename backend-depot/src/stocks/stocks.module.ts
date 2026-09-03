1e8d7531de1a18d974ce9a7c9cd5cd8a33203c2b
choubebebakon <choubebebakon@gmail.com>
2026-09-03 03:52:14 +0100

    feat(inventaire): register inventory API

--- backend-depot/src/stocks/stocks.module.ts
+++ backend-depot/src/stocks/stocks.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StocksService } from './stocks.service';
 import { StocksController } from './stocks.controller';
+import { InventaireService } from './inventaire.service';
+import { InventaireController } from './inventaire.controller';
 import { PrismaService } from '../prisma.service';
 import { AuditModule } from '../audit/audit.module';
 
 @Module({
   imports: [AuditModule],
-  controllers: [StocksController],
-  providers: [StocksService, PrismaService],
+  controllers: [StocksController, InventaireController],
+  providers: [StocksService, InventaireService, PrismaService],
 })
 export class StocksModule {}
